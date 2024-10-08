7461ac17c53b1db21ea02c4323e63146eacce358
TuanThanh0611 <nguyenhatuanthanh1901@gmail.com>
2024-10-08 22:04:54 +0700

    Ini

---
+++ spotify-clone-backend/src/test/java/com/ivo/spotify_clone_backend/SpotifyCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivo.spotify_clone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
