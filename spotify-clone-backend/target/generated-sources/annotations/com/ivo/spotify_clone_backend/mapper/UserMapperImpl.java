99e4237ff622d857365a2d04dde7a9f4822c25bf
TuanThanh0611 <nguyenhatuanthanh1901@gmail.com>
2024-10-12 22:07:58 +0700

    app(hone,layout,service,shared)

--- spotify-clone-backend/target/generated-sources/annotations/com/ivo/spotify_clone_backend/mapper/UserMapperImpl.java
+++ spotify-clone-backend/target/generated-sources/annotations/com/ivo/spotify_clone_backend/mapper/UserMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-10-09T23:18:51+0700",
+    date = "2024-10-10T14:58:51+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
--- spotify-clone-frontend/src/app/app.component.ts
+++ spotify-clone-frontend/src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {ToastService} from "./service/toast.service";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+import {NavigationComponent} from "./layout/navigation/navigation.component";
+import { HeaderComponent } from './layout/header/header.component';
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import{LibraryComponent} from "./layout/library/library.component";
+import {PlayerComponent} from "./layout/player/player.component";
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HttpClientModule,PlayerComponent,LibraryComponent, NgbModule,NavigationComponent,HeaderComponent,FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'spotify-clone-frontend';
+  private faIconLibrary = inject(FaIconLibrary);
+  
+  toastService = inject(ToastService);
+
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+  
 }
